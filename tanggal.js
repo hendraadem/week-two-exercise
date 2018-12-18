243ec94cd8e38b5d29361539110b558ae7aeba63
Ade Mahendra <hendraadem@gmail.com>
2018-12-18 20:26:45 +0700

    add 'tanggalan'

---
+++ tanggal.js
@@ -0,0 +1,45 @@
+var tanggal   = "12";
+var bulan     = "11";
+var tahun     = "1999";
+
+switch (bulan) {
+  case "1" :
+    bulan = "januari"
+  break;
+  case "2" :
+    bulan = "februari"
+  break;
+  case "3" :
+    bulan = "maret"
+  break;
+  case "4" :
+    bulan = "april"
+  break;
+  case "5" :
+    bulan = "mei"
+  break;
+  case "6" :
+    bulan = "juni"
+  break;
+  case "7" :
+    bulan = "juli"
+  break;
+  case "8" :
+    bulan = "agustus"
+  break;
+  case "9" :
+    bulan = "september"
+  break;
+  case "10" :
+    bulan = "oktober"
+  break;
+  case "11" :
+    bulan = "november"
+  break;
+  case "12" :
+    bulan = "desember"
+  break;
+
+}
+
+console.log(tanggal + ' ' + bulan + ' ' + tahun );
